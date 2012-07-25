6d40137cf32dddb09da1bf8139296c938607d1bc
Paul d'Hubert <contact@pauldhubert.com>
2012-07-25 14:01:06 +0200

    Updated template code to the one wrote for the ruby app.

--- src/blog/views/common.clj
+++ src/blog/views/common.clj
@@ -1,13 +1,40 @@
 (ns blog.views.common
-  (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+	(:use [noir.core :only [defpartial]]
+		  [hiccup.page :only [include-css include-js html5]]
+		  [hiccup.element :only [javascript-tag link-to]]))
+
+(defpartial typekit [kit]
+	(include-js (str "http://use.typekit.com/" kit ".js"))
+	[:script { :type "text/javascript" } "try{TypeKit.load();}catch(e){}" ])
+
+(def includes { :base (include-css "/css/base.css")
+				:skeleton (include-css "/css/skeleton.css")
+				:layout (include-css "/css/layout.css")
+				:typekit (typekit "bxp3bro")
+				:jquery (include-js "http://code.jquery.com/jquery-1.7.2.min.js") })
+
+(defpartial build-header [incls]
+	[:head
+		[:title "wwww.endou.fr"]
+		
+		[:meta {:charset "utf-8"}]
+		[:meta {:name "description" :content "A crazy programmer's blog !"}]
+		[:meta {:name "author" :content "Paul d'HUBERT"}]
+		
+		;For mobile web browsers 
+		[:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
+
+		(map #(get includes %) incls)])
+
+(defpartial header []
+	[:div {:class "header"}
+		(link-to "/" 
+			[:embed {:src "logo.svg" :type "image/svg+xml"}])])
 
 (defpartial layout [& content]
-            (html5
-              [:head
-               [:title "blog"]
-               (include-css "/css/reset.css")]
-              [:body
-               [:div#wrapper
-                content]]))
+	(html5
+		(build-header [:base :skeleton :layout :jquery :typekit])
+		[:body
+			(header)
+			content]))
 
--- src/blog/views/welcome.clj
+++ src/blog/views/welcome.clj
@@ -1,8 +1,14 @@
 (ns blog.views.welcome
-  (:require [blog.views.common :as common]
-            [noir.content.getting-started])
+  (:require [blog.views.common :as common])
   (:use [noir.core :only [defpage]]))
 
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to blog"]))
+(defpage "/" []
+	(common/layout
+		[:h3 "Endou"]
+		[:p "Hello world !"]
+		[:a {:href "#" :style "float: right;"}
+			"Read more..."]))
+
+; (defpage "/welcome" []
+;          (common/layout
+;            [:p "Welcome to blog"]))
