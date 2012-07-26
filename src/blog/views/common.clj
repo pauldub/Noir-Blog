eb5cc4369156d160cc9add3f87ed2ee9608eed28
Paul d'Hubert <contact@pauldhubert.com>
2012-07-26 17:53:14 +0200

    Ajout du code de tracking Google Analytics

--- src/blog/views/common.clj
+++ src/blog/views/common.clj
@@ -27,7 +27,9 @@
 		
 		;For mobile web browsers 
 		[:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
-		(map #(get includes %) incls)])
+		(map #(get includes %) incls)
+		(javascript-tag "var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-33686813-1']); _gaq.push(['_setDomainName', 'endou.fr']); _gaq.push(['_trackPageview']); (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();")
+		])
 
 (defpartial header []
 	[:div {:class "header"}
