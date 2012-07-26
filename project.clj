8985b51ba35390a220a6bd3923f1f918ff2e84d0
Paul d'Hubert <contact@pauldhubert.com>
2012-07-26 20:48:52 +0200

    Ajout de lein-daemon

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject blog "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "FIXME: write this!"            
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [org.clojure/core.cache "0.5.0"]
-                           [cheshire "4.0.1"]]
+                           [cheshire "4.0.1"]
+                           [lein-daemon "0.4.2"]]
             :main blog.server)
 
