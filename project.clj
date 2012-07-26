a2bbed384be41d98e7b6120058bd865e733851ee
Paul d'Hubert <contact@pauldhubert.com>
2012-07-26 20:56:16 +0200

    Modification des dependances.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [org.clojure/core.cache "0.5.0"]
-                           [cheshire "4.0.1"]
-                           [lein-daemon "0.4.2"]]
+                           [cheshire "4.0.1"]]
+			:dev-dependencies [[lein-daemon "0.4.2"]]
            	:daemon {
-           		:name-of-service {
+           		:blogd {
            			:ns blog.server
            			:pidfile "/var/run/blogd"}}
             :main blog.server)
