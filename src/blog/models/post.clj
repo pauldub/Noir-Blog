8474183a050485e1c663a861860412ac0256b113
Paul d'Hubert <contact@pauldhubert.com>
2012-07-26 16:41:34 +0200

    Mise en cache simplissime des meta données.

--- src/blog/models/post.clj
+++ src/blog/models/post.clj
@@ -9,25 +9,30 @@
 
 (def path (str "/home/paul/projects/noir/blog/posts"))
 
-(def meta-files 
-	(for [f (file-seq (directory path)) :when (re-find #"^[\w|\d -]+.json$" (.getName f))]
-      f))
+(defn meta-files []
+	(def cached-meta-files (get C :meta_files))
+	(if (= cached-meta-files nil)
+		(let [metas (for [f (file-seq (directory path)) :when (re-find #"^[\w|\d -]+.json$" (.getName f))]
+      		f)]
+			(cache-set :meta_files metas))
+		cached-meta-files))
 
 (defn get-content [f]
 	(if (not (nil? f))
 		(slurp (str path "/" f))))
 
 (defn get-all []
-	(for [f meta-files :when (not (.isDirectory f))]
+	(for [f (meta-files) :when (not (.isDirectory f)) :when (.exists f)]
 		(decode (slurp f) true)))
 
-(defn cache-set [permalink f]
-	(def C (-> C (assoc (keyword permalink) (decode (slurp f) true))))
-	(decode (slurp f) true))
+(defn cache-set [key value]
+	(println (str "[CACHE] New value for " key " : " [value]))
+	(def C (-> C (assoc key value)))
+	value)
 
 (defn get-one [permalink]
 	(def cached (get C (keyword permalink)))
 	(if (= cached nil)
-		(for [f meta-files :when (not (.isDirectory f)) :when (= (str (second (re-matches #"^([\w|\d -]+).md$" permalink)) ".json") (.getName f))]
-			(cache-set permalink f))
+		(for [f (meta-files) :when (not (.isDirectory f)) :when (= (str (second (re-matches #"^([\w|\d -]+).md$" permalink)) ".json") (.getName f))]
+			(cache-set (keyword permalink) (decode (slurp f) true)))
 		[cached]))
