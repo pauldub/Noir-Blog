362d13525b789bd0ed0aa6d3bc46756f465a17a9
Paul d'Hubert <contact@pauldhubert.com>
2012-07-26 21:08:33 +0200

    Réorganisation du projet.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject blog "0.1.0-SNAPSHOT"
             :description "My personnal static file weblog."            
+            
+			:dev-dependencies [[lein-daemon "0.4.2"]]
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [org.clojure/core.cache "0.5.0"]
                            [cheshire "4.0.1"]]
-			:dev-dependencies [[lein-daemon "0.4.2"]]
            	:daemon {
            		:blogd {
            			:ns blog.server
            			:pidfile "blogd.pid"}}
+
             :main blog.server)
 
