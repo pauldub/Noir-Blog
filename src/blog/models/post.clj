aa9c8fe75bf3f3362a5e09db834628fd1d10bba8
Zach Holman <zach@zachholman.com>
2012-07-27 16:06:44 +0200

    Refactoring some var names.

--- src/blog/models/post.clj
+++ src/blog/models/post.clj
@@ -4,34 +4,37 @@
 			  [clj-time.core :as time]
 			  [clj-time.format :as time-format]))
 
-(def C (cache/ttl-cache-factory (* 1000 60 5) {}))
+(def cache (cache/ttl-cache-factory (* 1000 1) {}))
 
 (defn directory [path]
 	(clojure.java.io/file path))
 
-(def path (str "/home/paul/projects/noir/blog/posts"))
+(def meta-files-path (str "/home/paul/projects/noir/blog/posts"))
 
 (defn cache-set [key value]
-	(println (str "[CACHE] New value for " key " : " [value]))
-	(def C (-> C (assoc key value)))
+	(println (str "[CACHE] New value for " key "\n"))
+	(def cache (-> cache (assoc key value)))
 	value)
 
 (defn meta-files []
-	(def cached-meta-files (get C :meta_files))
+	(def cached-meta-files (get cache :meta_files))
 	(if (= cached-meta-files nil)
-		(let [metas (for [file (file-seq (directory path)) :when (not (.isDirectory file)) :when (.exists file) :when (re-find #"^[\w|\d -]+.json$" (.getName file))]
+		(let [metas (for [file (file-seq (directory meta-files-path)) 
+				:when (not (.isDirectory file)) 
+				:when (.exists file) 
+				:when (re-find #"^[\w|\d -]+.json$" (.getName file))]
       		(let [json (decode (slurp file) true)]
       			[(keyword (json :content)) json]))]
 			(into {} (cache-set :meta_files metas)))
 		(into {} cached-meta-files)))
 
 (defn get-content [filename]
-	(let [cached-content (get C (keyword filename))]
+	(let [cached-content (get cache (keyword filename))]
 		(if (= cached-content nil)
-			(cache-set (keyword filename) (slurp (str path "/" filename)))
+			(cache-set (keyword filename) (slurp (str meta-files-path "/" filename)))
 			cached-content)))
 
-(defn comp-cached-meta-files
+(defn comp-cached-meta-files-date
 	[el1 el2]
 		(let [[filename1 metas1] el1 [filename2 metas2] el2]
 			(time/after? 
@@ -39,7 +42,7 @@
 				(time-format/parse (time-format/formatter "dd/MM/yy") (metas2 :date)))))
 
 (defn get-all []
-	(vals (sort (comp comp-cached-meta-files) (meta-files))))
+	(vals (sort (comp comp-cached-meta-files-date) (meta-files))))
 
 (defn get-one [permalink]
 	((meta-files) (keyword permalink)))
