04a2461d1155258218172f6715e2a27b7addf0cc
Paul d'Hubert <contact@pauldhubert.com>
2012-09-01 12:51:30 +0000

    Fixed fabfile.py and testing it..

--- fabfile.py
+++ fabfile.py
@@ -2,15 +2,9 @@ from fabric.api import *
 import deploy
 
 @task
-def staging():
-    env.hosts = ['paul@endou.fr']
-    env.branch = 'develop'
-    env.remote = 'staging'
-    env.path   = '/home/paul/projets/noir/blog/resources/public/static'
-
-@task
 def production():
-    env.hosts = ['tabbakk@pauldhubert.com']
+    env.hosts = ['paul@endou.fr']
     env.branch = 'master'
-    env.remote = 'production'
-    env.path   = '/home/tabbakk/webapps/main'
+    env.remote = 'origin'
+    env.path   = '/home/paul/projets/noir/blog'
+
