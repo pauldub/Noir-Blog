c57bc28cfb5a1029edf7f20ca4d32326583a9e8b
Paul d'Hubert <contact@pauldhubert.com>
2012-09-01 13:15:21 +0000

    First test of fabric init task

--- deploy.py
+++ deploy.py
@@ -1,4 +1,11 @@
 from fabric.api import task, local, env, cd, run
+from fabric.contrib.files import exists
+from fabric.contrib.console import confirm
+
+@task
+def init():
+    run('git init')
+    run('git add remote origin ' + env.remote_string)
 
 @task
 def commit():
@@ -8,9 +15,12 @@ def commit():
 def push():
     local('git push ' + env.remote + ' ' + env.branch)
 
+
 @task
 def pull():
     with cd(env.deploy_path):
+        if not exists('.git') and confirm('The current directory is not a git repo, run git init?'):
+            init()
         run('git checkout ' + env.branch)
         run('git pull origin ' + env.branch)
 
--- fabfile.py
+++ fabfile.py
@@ -8,5 +8,6 @@ def production():
     env.key_filename = '/home/paul/.ssh/id_rsa'
     env.branch = 'master'
     env.remote = 'origin'
-    env.deploy_path   = '~/projects/noir/blog'
+    env.remote_string = 'https://github.com/pauldub/Noir-Blog.git'
+    env.deploy_path   = '~/projects/noir/endou'
 
