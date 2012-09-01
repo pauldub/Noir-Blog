89a64c86bb485e249113515f7862071b7bec3905
Paul d'Hubert <contact@pauldhubert.com>
2012-09-01 13:26:43 +0000

    Fixed remote and branch handling in fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,14 @@
 from fabric.api import *
 import deploy
 
+env.branch = 'master'
+env.remote = 'origin'
+
 @task
 def production():
     env.hosts = ['endou.fr']
     env.user  = 'paul'
     env.key_filename = '/home/paul/.ssh/id_rsa'
-    env.branch = 'master'
-    env.remote = 'origin'
     env.remote_string = 'https://github.com/pauldub/Noir-Blog.git'
     env.deploy_path   = '~/projects/noir/endou'
 
