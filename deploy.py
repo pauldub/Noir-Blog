e337f91647721fb044581a715bc4b141454f72f0
Paul d'Hubert <contact@pauldhubert.com>
2012-09-01 13:39:23 +0000

    Updated fabric deploy.py module with methods to control server process and more useful methods

--- deploy.py
+++ deploy.py
@@ -17,13 +17,38 @@ def push():
     local('git push ' + env.remote + ' ' + env.branch)
 
 
-@task(alias='update')
+@task
 def pull():
     with cd(env.deploy_path):
         if not exists('.git') and confirm('The current directory is not a git repo, run git init?'):
             init()
         run('git checkout ' + env.branch)
         run('git pull origin ' + env.branch)
+
+@task
+def restart():
+    run('sudo rc.d restart blogd')
+
+@task
+def start():
+    run('sudo rc.d start blogd')
+
+@task
+def stop():
+    run('sudo rc.d stop blogd')
+
+@task
+def status():
+    run('sudo rc.d status blogd')
+
+@task
+def logs():
+    run('sudo tail -f /var/log/blogd.log')
+
+@task
+def update():
+    commit()
+    push()
 
 @task(default=True)
 def full_deploy():
