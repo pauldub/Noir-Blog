25eb9116fa9b90d75d04213a7765a39e37341258
Paul d'Hubert <contact@pauldhubert.com>
2012-09-01 13:01:02 +0000

    Test for deploy_path

--- fabfile.py
+++ fabfile.py
@@ -8,5 +8,5 @@ def production():
     env.key_filename = '/home/paul/.ssh/id_rsa'
     env.branch = 'master'
     env.remote = 'origin'
-    env.deploy_path   = '/home/paul/projets/noir/blog'
+    env.deploy_path   = '~/projets/noir/blog'
 
